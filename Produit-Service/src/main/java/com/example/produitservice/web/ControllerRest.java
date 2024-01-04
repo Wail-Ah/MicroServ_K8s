632566de69a5eeb886f505a98b2aacafdecd2be1
Wail-Ah <wail.aithioua@um5r.ac.ma>
2024-01-04 21:30:15 +0100

    V1

---
+++ Produit-Service/src/main/java/com/example/produitservice/web/ControllerRest.java
@@ -0,0 +1,59 @@
+package com.example.produitservice.web;
+
+import com.example.produitservice.config.GlobalConfig;
+import com.example.produitservice.entities.Produit;
+import com.example.produitservice.repositories.ProduitRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+//@RequestMapping("/api")
+public class ControllerRest {
+    @Autowired
+    ProduitRepository produitRepository;
+
+    @Autowired
+    GlobalConfig globalConfig;
+
+    @GetMapping("/globalConfig")
+    public  GlobalConfig globalConfig(){
+        return globalConfig;
+    }
+
+    //GET to return all the products
+    @GetMapping("/produits")
+    public List<Produit> getAllProduits(){
+        return produitRepository.findAll();
+    }
+
+    //GET to return product by it's id
+    @GetMapping("/produits/{id}")
+    public Produit getByIdProduit(@PathVariable Integer id){
+        Produit produit = new Produit();
+        produit=produitRepository.findById(id).get();
+        return produit;
+    }
+
+    //POST to add a new product
+    @PostMapping("/produits")
+    public void ajouter(@RequestBody Produit produit){
+        produitRepository.save(produit);
+    }
+
+    //PUT to update a product
+    @PutMapping("/produits/{id}")
+    public void updatet(@PathVariable Integer id,@RequestBody Produit produit){
+        Produit produit1=produitRepository.findById(id).get();
+        BeanUtils.copyProperties(produit,produit1);
+        produitRepository.save(produit1);
+    }
+
+    //DELETE to delete a product from the database
+    @DeleteMapping("/produit/{id}")
+    public void delete(@PathVariable Integer id){
+        produitRepository.deleteById(id);
+    }
+}
